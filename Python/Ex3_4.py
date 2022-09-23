0985a3937b6209c2195598f6a1c62fc017c93276
Ilya Anikeyenka <71770143+ianikeyenka@users.noreply.github.com>
2022-09-23 16:22:14 +0300

    Delete Ex3_4.py

--- Python/Ex3_4.py
+++
@@ -1,3 +0,0 @@
-command1 = 'switchport trunk allowed vlan 1,3,10,20,30,100'
-command2 = 'switchport trunk allowed vlan 1,3,100,200,300'
-print(set((command1.split())[-1].split(',')).intersection(set((command2.split())[-1].split(','))))
