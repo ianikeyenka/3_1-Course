dcfdaa60d86dc0569b7fce6e379b5d5e31f792c6
Ilya Anikeyenka <71770143+ianikeyenka@users.noreply.github.com>
2022-09-23 16:23:18 +0300

    Delete Ex3_9.py

--- Python/Ex3_9.py
+++
@@ -1,5 +0,0 @@
-num_list = [10, 2, 30, 100, 10, 50, 11, 30, 15, 7]
-word_list = ['python', 'ruby', 'perl', 'ruby', 'perl', 'python', 'ruby', 'perl']
-key = 'ruby'
-value = word_list
-print(len(value) - value[::-1].index(key) - 1)
