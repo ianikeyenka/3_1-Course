ea8f283f534eaa62e75394370dfc9d7bd2f9a0e5
Ilya Anikeyenka <71770143+ianikeyenka@users.noreply.github.com>
2022-11-16 13:46:29 +0300

    Update Lab6_2.cs

--- C#/Lab6_2.cs
+++ C#/Lab6_2.cs
@@ -2,34 +2,30 @@ using System.Collections;
 
 namespace lab6_2;
 
-interface GeometricFigure
+interface IGeometricFigure
 { 
     double Space { get; } 
     string Information();
 }
 
-public class Square : GeometricFigure 
+public class Square : IGeometricFigure 
 { 
     private int side;
-    
     public Square(int side) 
     { 
         this.side = side;
     }
 
     public int Side { get { return side; }}
-    
     public int Perimeter { get { return 4 * side; } } 
-    
     public double Space { get { return (side * side); } }
-    
     public string Information() 
     { 
         return string.Format("\t\tSquare\nSide - {0}\nPerimeter - {1}\nSpace - {2}\n", side, Perimeter, Space);
     }
 }
 
-public class Circle : GeometricFigure 
+public class Circle : IGeometricFigure 
 { 
     private int radius; 
     private string color;
@@ -45,47 +41,40 @@ public class Circle : GeometricFigure
     public string Color { get { return color; } }
 
     public double Space { get { return Math.PI * Math.Pow(radius, 2); } } 
-    
     public string Information() 
     { 
         return string.Format("\t\tCircle\nRadius - {0}\nColor - {1}\nSpace - {2:F3}\n", radius, color, Space);
     }
 }
 
-class ArrayOfFigures : IEnumerable 
-{ 
-    private string[] geometricFigure;
-    
-    public ArrayOfFigures(string[] geometricFigure) 
-    { 
-        this.geometricFigure = geometricFigure;
+class ArrayOfFigures: IEnumerable
+{
+    IGeometricFigure[] geometricFigure;
+ 
+    public ArrayOfFigures(params IGeometricFigure[] gf)
+    {
+        geometricFigure = gf;
     }
-    
-    IEnumerator IEnumerable.GetEnumerator() 
-    { 
+        
+    IEnumerator IEnumerable.GetEnumerator()
+    {
         return geometricFigure.GetEnumerator();
-    } 
+    }
 }
 
 public class Program 
 { 
-    static void Main(string[] args) 
-    { 
-        Square sqare1 = new Square(3); 
-        Square sqare2 = new Square(4);
-        Circle circle1 = new Circle(3, "green"); 
-        Circle circle2 = new Circle(4, "blue");
-        string[] informFigure = 
+    static void Main(string[] args)
+    {
+        ArrayOfFigures figure = new ArrayOfFigures(
+            new Square(3),
+            new Square(4),
+            new Circle(3, "green"),
+            new Circle(4, "blue"));
+        
+        foreach (IGeometricFigure value in figure) 
         { 
-            sqare1.Information(), 
-            sqare2.Information(), 
-            circle1.Information(), 
-            circle2.Information(),
-        };
-        ArrayOfFigures array = new ArrayOfFigures(informFigure); 
-        foreach (string value in informFigure) 
-        { 
-            Console.WriteLine(value);
+            Console.WriteLine(value.Information());
         }
     }
 }
